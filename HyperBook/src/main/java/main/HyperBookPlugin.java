1dad8687558e6613a23e7f17a58347c7445f5870
Zyq_Playz <alifabdullahmd92@gmail.com>
2026-05-13 21:51:25 +0600

    Implement HyperBook plugin initialization

--- HyperBook/src/main/java/main/HyperBookPlugin.java
+++ HyperBook/src/main/java/main/HyperBookPlugin.java
@@ -1,1 +1,20 @@
+package com.hyperbook;
 
+import com.hyperbook.commands.HyperBookCommand;
+import com.hyperbook.listeners.GUIListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HyperBookPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        saveDefaultConfig();
+
+        getCommand("hyperbook").setExecutor(new HyperBookCommand());
+
+        getServer().getPluginManager().registerEvents(new GUIListener(), this);
+
+        getLogger().info("HyperBook Enabled!");
+    }
+}
