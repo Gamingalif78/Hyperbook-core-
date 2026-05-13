9e12fb7962c009be07cd940df588a3888823738c
Zyq_Playz <alifabdullahmd92@gmail.com>
2026-05-13 21:53:04 +0600

    Implement GUIListener for inventory click events

--- HyperBook/src/main/java/listeners/GUIListener.java
+++ HyperBook/src/main/java/listeners/GUIListener.java
@@ -1,1 +1,26 @@
+package com.hyperbook.listeners;
 
+import com.hyperbook.gui.HyperBookGUI;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class GUIListener implements Listener {
+
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+
+        if (!e.getView().getTitle().equals(HyperBookGUI.TITLE)) return;
+
+        e.setCancelled(true);
+
+        if (e.getCurrentItem() == null) return;
+
+        switch (e.getCurrentItem().getType()) {
+
+            case BOOK -> e.getWhoClicked().sendMessage("§aEquipped HyperBook!");
+            case ENCHANTED_BOOK -> e.getWhoClicked().sendMessage("§eUnequipped HyperBook!");
+            case BARRIER -> e.getWhoClicked().sendMessage("§cYou don't own this!");
+        }
+    }
+}
