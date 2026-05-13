6534dd34410fbd1c6b2e7f7d70ea9d776a740b2b
Zyq_Playz <alifabdullahmd92@gmail.com>
2026-05-13 21:53:54 +0600

    Add ItemBuilder class for creating ItemStacks

--- HyperBook/src/main/java/utils/ItemBuilder.java
+++ HyperBook/src/main/java/utils/ItemBuilder.java
@@ -1,1 +1,23 @@
+package com.hyperbook.utils;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class ItemBuilder {
+
+    public static ItemStack create(Material material, String name, String... lore) {
+
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName(name);
+        meta.setLore(Arrays.asList(lore));
+
+        item.setItemMeta(meta);
+
+        return item;
+    }
+}
