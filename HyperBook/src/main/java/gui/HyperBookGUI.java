5026d04dafe7388512714fa275709df6bec32a96
Zyq_Playz <alifabdullahmd92@gmail.com>
2026-05-13 21:52:20 +0600

    Implement HyperBookGUI for player inventory management

--- HyperBook/src/main/java/gui/HyperBookGUI.java
+++ HyperBook/src/main/java/gui/HyperBookGUI.java
@@ -1,1 +1,39 @@
+package com.hyperbook.gui;
 
+import com.hyperbook.utils.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class HyperBookGUI {
+
+    public static final String TITLE = "§6HyperBooks";
+
+    public static void open(Player player) {
+
+        Inventory gui = Bukkit.createInventory(null, 27, TITLE);
+
+        gui.setItem(11, ItemBuilder.create(
+                Material.BOOK,
+                "§eMob Power HyperBook",
+                "§7Status: §cNot Equipped",
+                "§eClick to Equip"
+        ));
+
+        gui.setItem(13, ItemBuilder.create(
+                Material.BARRIER,
+                "§cLocked HyperBook",
+                "§7Not Obtained"
+        ));
+
+        gui.setItem(15, ItemBuilder.create(
+                Material.ENCHANTED_BOOK,
+                "§aMining HyperBook",
+                "§7Status: §aEquipped",
+                "§eClick to Unequip"
+        ));
+
+        player.openInventory(gui);
+    }
+}
