a6eb03913294bb9f46f91a37687941aadd42f818
Zyq_Playz <alifabdullahmd92@gmail.com>
2026-05-13 21:47:42 +0600

    Update HyperBookCommand.java

--- HyperBook/src/main/java/commands/HyperBookCommand.java
+++ HyperBook/src/main/java/commands/HyperBookCommand.java
@@ -1,1 +1,22 @@
+package com.hyperbook.commands;
 
+import com.hyperbook.gui.HyperBookGUI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HyperBookCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this!");
+            return true;
+        }
+
+        HyperBookGUI.open(player);
+        return true;
+    }
+}
